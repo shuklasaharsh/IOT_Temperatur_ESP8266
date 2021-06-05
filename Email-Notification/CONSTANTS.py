c05124e67d97e5cd8220be822772fc7145e85e04
Saharsh Shukla <67519229+shuklasaharsh@users.noreply.github.com>
2021-06-05 13:44:01 +0530

    Update CONSTANTS.py

--- Email-Notification/CONSTANTS.py
+++ Email-Notification/CONSTANTS.py
@@ -4,5 +4,5 @@ import pathlib
 
 class CONSTANTS(Enum):
     path = str(pathlib.Path(__file__).parent.absolute())
-    email = "shukla.saharsh7.work@gmail.com"
-    password = "DeltaS>0"
+    email = "YourEmail"
+    password = "YourPassword"
