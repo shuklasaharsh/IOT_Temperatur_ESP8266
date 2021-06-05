1a2d284190c6a6a06db1f0a53b56c2a971d377a8
Saharsh Shukla <SaharshShukla@Saharshs-Macbook-Pro.local>
2021-06-05 14:27:32 +0530

    Update Requirements.txt

--- Email-Notification/main.py
+++ Email-Notification/main.py
@@ -1,15 +1,16 @@
 import pandas as pd
 import getData as gd
 import createMail as cm
-
+import time
 gd.saveData()
 
 df = pd.read_csv('./1.csv')
 temperature, time = gd.splitData(df)
-address = input("Enter Email: ")
+address = "saharsh.shukla2018@vitstudent.ac.in"
 data = [time, temperature]
 cm.sendMail(address, data)
-
+a = time.time
+print("Mail sent at time: ", a)
 
 
 
